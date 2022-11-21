e4bfc8e649d3d9cde48484d6a7db6475965830ef
Connor McLaughlan <cmclaughlan15@gmail.com>
2022-11-21 15:12:28 -0700

    DB init

--- Source/load_json.py
+++ Source/load_json.py
@@ -1,5 +1,6 @@
-import sys
+import sys, json
 from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 
 def main():
     if len(sys.argv) > 2:
@@ -18,8 +19,35 @@ def init_db(filename, port):
     assert type(filename) is str
     assert type(port) is int
 
-    client = MongoClient("127.0.0.1", port)
-    db = self.client["dblp"]
+    client = MongoClient("127.0.0.1", port, serverSelectionTimeoutMS=1000)
+
+    try:
+        client.admin.command('ismaster')
+    except ConnectionFailure:
+        print("Connection to MongoDB server failed!")
+        exit()
+
+    db = client["291db"]
+
+    # See if collection exists
+    collist = db.list_collection_names()
+    if "dblp" in collist:
+        # drop db and reinit
+        db.dblp.drop()
+
+    # init collection
+    db["dblp"]
+    init_collection_from_file(filename, db)
+
+
+def init_collection_from_file(filename, db):
+    fo = open(filename, 'r')
+
+    line = fo.readline()
+    while line:
+        print(line)
+        ret = db.dblp.insert_one(json.loads(line))
+        line = fo.readline()
 
 
 
